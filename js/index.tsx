1c22484e785f8b3157aa3e88accc81fa8ba082cb
Anna <40352117+petulant-annie@users.noreply.github.com>
2019-02-01 14:02:07 +0200

    Add files via upload

--- js/index.tsx
+++ js/index.tsx
@@ -2,47 +2,59 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 const speed = 3000;
-const melt = true;
-const dotAppear = true;
+const meltAppear = true;
+const dotAppear = false;
+const slideshowAppear = false;
 
 interface ISlider {
   speed: number;
-  melt: boolean;
   dotAppear: boolean;
+  meltAppear: boolean;
+  slideshowAppear: boolean;
 }
 
-class Slider extends React.Component<ISlider, { count: number }> {
+class Slider extends React.Component<ISlider, { current: number }> {
   speed: number;
   slides: any; // React.ReactElement<any>[]
-  melt: boolean;
   slideStyle: string;
   dotAppear: boolean;
-  childrenArr: any;
+  meltAppear: boolean;
+  slideshowAppear: boolean;
 
   constructor(props: ISlider) {
     super(props);
     this.speed = props.speed || 3000;
-    this.melt = props.melt || false;
     this.dotAppear = props.dotAppear || false;
+    this.meltAppear = props.meltAppear || false;
+    this.slideshowAppear = props.slideshowAppear || false;
     this.slideStyle = 'active';
-    this.childrenArr;
 
     this.state = {
-      count: 0,
+      current: 0,
     };
   }
 
   autoplay() {
     setTimeout(() => {
-      this.state.count + 1 < this.slides.length ?
-        this.setState({ count: this.state.count + 1 }) :
-        this.setState({ count: 0 });
+      this.state.current + 1 < this.slides.length ?
+        this.setState({ current: this.state.current + 1 }) :
+        this.setState({ current: 0 });
     }, this.speed);
   }
 
-  dotAppearSlide(): void {
-    this.slideStyle = 'dot-current';
+  animate() {
+    if (this.dotAppear) return 'dot-current';
+    if (this.meltAppear) return 'melt-current';
+    if (this.slideshowAppear) return 'slideshow-current';
+
+    return 'active';
   }
+
+  // classNameProps(index: number) {
+  //   const a = (index === this.state.current ? `${this.animate()}` : 'slide');
+
+  //   return a;
+  // }
 
   getSlides() {
     const { children } = this.props;
@@ -50,10 +62,8 @@ class Slider extends React.Component<ISlider, { count: number }> {
     this.slides = React.Children.map(children, (child: JSX.Element, index: number) =>
       React.cloneElement(child, {
         key: index,
-        className: (index === this.state.count ? `${this.slideStyle}` : 'slide'),
+        className: (index === this.state.current ? `${this.animate()}` : 'slide'),
       }));
-
-    // console.log(this.slides[0].props.className);
 
     return (
       <div id="slider">
@@ -64,7 +74,6 @@ class Slider extends React.Component<ISlider, { count: number }> {
 
   render() {
     this.autoplay();
-    if (this.dotAppear) this.dotAppearSlide();
 
     return (
       this.getSlides()
@@ -75,8 +84,9 @@ class Slider extends React.Component<ISlider, { count: number }> {
 const slider = (
   <Slider
     speed={speed}
-    melt={melt}
+    meltAppear={meltAppear}
     dotAppear={dotAppear}
+    slideshowAppear={slideshowAppear}
   >
     <div>
       <img src={require('../assets/images/lake.jpg')} title="Lake" />
